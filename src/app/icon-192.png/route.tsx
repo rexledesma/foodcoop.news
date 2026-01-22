e0a369a25226a03ca353ce334929725454825fc9
Rex Ledesma <rex@rexledesma.com>
2026-01-22 15:57:59 -0500

    feat: update favicon and apple touch icon to newspaper

---
+++ src/app/icon-192.png/route.tsx
@@ -0,0 +1,27 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export async function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 150,
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "white",
+        }}
+      >
+        📰
+      </div>
+    ),
+    {
+      width: 192,
+      height: 192,
+    }
+  );
+}
