227ba96579d73d99c129a37efa352878cb96aaf3
Rex Ledesma <rex@rexledesma.com>
2026-01-25 23:44:25 -0500

    fix: use suspense boundary for signup form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { SignUpForm } from "@/components/SignUpForm";
 
 export const metadata = {
@@ -5,5 +6,9 @@ export const metadata = {
 };
 
 export default function SignUpPage() {
-  return <SignUpForm />;
+  return (
+    <Suspense>
+      <SignUpForm />
+    </Suspense>
+  );
 }
