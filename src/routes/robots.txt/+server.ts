a1eea3d39be8c01b16d2921ef4f325c698fff676
Rex Ledesma <rex@rexledesma.com>
2026-02-24 18:58:34 -0500

    chore(seo): add robots.txt

---
+++ src/routes/robots.txt/+server.ts
@@ -0,0 +1,26 @@
+import { env } from '$env/dynamic/private';
+
+function toAbsoluteUrl(origin: string, pathname: string): string {
+  const normalizedOrigin = origin.replace(/\/+$/, '');
+  return `${normalizedOrigin}${pathname}`;
+}
+
+export function GET({ url }: { url: URL }) {
+  const origin = env.SITE_URL || url.origin;
+  const sitemapUrl = toAbsoluteUrl(origin, '/sitemap.xml');
+  const body = `User-agent: *
+Allow: /
+Disallow: /api/
+Disallow: /login
+Disallow: /signup
+
+Sitemap: ${sitemapUrl}
+`;
+
+  return new Response(body, {
+    headers: {
+      'cache-control': 'public, max-age=3600',
+      'content-type': 'text/plain; charset=utf-8',
+    },
+  });
+}
